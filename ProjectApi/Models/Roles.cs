30c9eec6e2bc15d2c3298d56eeabde4c198423ff
Kwabena Twumasi <ktwum721@gmail.com>
2023-07-30 21:58:39 +0000

    First commit for a New Class

---
+++ ProjectApi/Models/Roles.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectApi.Models
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public RoleId role { get; set; }
+    }
+
+    public enum RoleId
+    {
+        CollegeOfScience,
+        CollegeOfNaturalScience,
+        CollegeOfHumanities,
+        CollegeOfSocialSciences,
+        CollegeOfHealth,
+        CollegeOfArts
+    }
+}
