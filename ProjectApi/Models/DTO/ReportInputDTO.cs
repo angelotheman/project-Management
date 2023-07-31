9d40510780ba99a307e5ef6f62935f88f7b6cd81
Kwabena Twumasi <ktwum721@gmail.com>
2023-07-31 16:11:28 +0000

    Report DTO added for data input from user

---
+++ ProjectApi/Models/DTO/ReportInputDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectApi.Models.DTO
+{
+    public class ReportInputDTO
+    {
+        public string FaultDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public Category FaultCategory { get; set; }
+        public string Location { get; set; }
+    }
+}
